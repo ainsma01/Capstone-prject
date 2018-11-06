46b6d34c04fea59e2f4e3e5dce0aa802c0e0e7c6
Healdb <healdb@healdb.com>
2018-11-06 15:16:18 -0500

    Create basic package check functionality
    Basic functions to check if given student ID num has any packages. Still need to parse the returned JSON for relevant data. Right now it just turns all JSON into a string and displays it

--- Package.js
+++ Package.js
@@ -1,18 +1,66 @@
 import React, {Component} from 'react';
-import {View, Text, StyleSheet} from 'react-native';
+import {View, Text, TextInput, StyleSheet} from 'react-native';
 
-import { Icon, Button, Container, Header, Content, Left} from 'native-base';
+import { Event, Icon, Button, Container, Header, Content, Left} from 'native-base';
 
 export default class Package extends Component {
+    constructor(props) {
+    super(props);
+      this.state = {text: ""}; 
+      this.handleChange = this.handleChange.bind(this);
+      this.checkIfHasPackage = this.checkIfHasPackage.bind(this);
+    }
+
+    checkIfHasPackage(studentID) {
+      return fetch('https://api.cloudmine.me/v1/app/6c7bfc297efc4ab294bbf22ed23c7701/run/SearchGettysburgPackages?studentid='+studentID, {
+        method: 'GET',
+        headers: {
+          Accept: 'application/json',
+          'Content-Type': 'application/json',
+          'X-CloudMine-ApiKey': 'fb60033770f44aa6ab8577e907223d6c',
+        },
+      })
+          .then((response) => response.json())
+          //If the API returned JSON data
+          .then((responseJson) => {
+            //console.log(responseJson.result)
+            //Set the text view to be the repsonse JSON
+            //Will need to do more parsing here later
+            var responseString = JSON.stringify(responseJson)
+              this.setState({
+                text: JSON.stringify(responseJson),
+              });
+
+          })
+          //
+          .catch((error) =>{
+            console.error(error);
+          });
+    }
+
+    handleChange(event) {
+      var ID = event.nativeEvent.text;
+      //If text is the length of an ID
+      if(ID.length == 7){
+        this.checkIfHasPackage(ID);
+      }
+    }
+
     render() {
         return (
             <View style = {styles.container}>
-
+              <Text>
+                {this.state.text}
+              </Text>
+              <TextInput
+                style={{height: 50, width:200}}
+                placeholder="Enter Student ID here."
+                onChange={this.handleChange}
+              />
              </View>
         );
     }
 }
-
 const styles = StyleSheet.create({
     container:{
         flex: 1,
