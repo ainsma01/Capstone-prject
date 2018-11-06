919e6f669d29f5189ffcccfbbf1e1d9c20da243e
Healdb <healdb@healdb.com>
2018-11-06 15:17:20 -0500

    Added dining services link and renamed things

--- App.js
+++ App.js
@@ -1,11 +1,12 @@
 import React, {Component} from 'react';
-import { StyleSheet, Text, View, ImageBackground} from 'react-native';
+import { Button,StyleSheet, Text, View, ImageBackground} from 'react-native';
 import Login from './components/Login';
 import {createDrawerNavigator} from 'react-navigation'
 import HomeScreen from './HomeScreen'
 import Settings from './Settings'
 import Community from './Community'
 import Dining from './Dining'
+import DiningServices from './DiningServices'
 import Package from './Package'
 
 export default class App extends React.Component {
@@ -15,18 +16,23 @@ export default class App extends React.Component {
     );
   }
 }
-
+const navigationOptions = function(props) {
+  return {
+    headerLeft: <Button onPress={() => props.navigation.navigate('DrawerOpen')} title= "=" />
+  }
+};
 const AppDrawerNavigator = createDrawerNavigator({
   Login: Login,
   Home: HomeScreen,
-  Dining: Dining,
+  "Dining Menus": Dining,
+  "Dining Services": DiningServices,
   Community: Community,
-  Package: Package,
+  "Package Check": Package,
   Settings: Settings
   
 })
 
-styles = StyleSheet.create({
+const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: 'blue',
