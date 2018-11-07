9c6dee84c1bac9c2f2be8882a1baca4b652b5062
Healdb <healdb@healdb.com>
2018-11-07 11:16:07 -0500

    Add files via upload

--- components/LoginForm.js
+++ components/LoginForm.js
@@ -1,26 +1,59 @@
 import React, {Component} from 'react';
 import {StyleSheet, View, TextInput, TouchableOpacity, Text, Alert} from 'react-native';
 import { createStackNavigator } from 'react-navigation'; 
+import { withNavigation } from 'react-navigation';
 
 import HomeScreen from '../HomeScreen'
 
-export default class LoginForm extends Component {
+class LoginForm extends Component {
+  constructor(props) {
+    super(props);
+      this.state = {username: "", password: ""}; 
+      this.handleUserChange = this.handleUserChange.bind(this);
+      this.handlePassChange = this.handlePassChange.bind(this);
+      this.handleLogin = this.handleLogin.bind(this);
+    }
+    handlePassChange(event){
+      var pass = event.nativeEvent.text;
+      this.setState({
+        password: pass,
+      });
+    }
+    handleUserChange(event){
+      var user = event.nativeEvent.text;
+      this.setState({
+        username: user,
+      });
+    }
+    handleLogin(event){
+      var username = this.state.username
+      var password = this.state.password
+      if(username=="admin" && password=="admin"){
+        //this.props.navigator('DrawerStack')
+        console.log("yep");
+      }
+    }
     render() {
+        const { navigate } = this.props.navigation;
         return (
             <View style = {styles.container}>
                 <TextInput 
                     placeholder = "Username"
                     placeholderTextColor = "white"
+                    onChange={this.handleUserChange}
                     style = {styles.input}
                 />
                 <TextInput
                     placeholder = "Password"
                     placeholderTextColor = "white"
                     secureTextEntry
+                    onChange={this.handlePassChange}
                     style = {styles.input}
                 />
-
-                <TouchableOpacity style = {styles.buttonContainer} onPress = {() => Alert.alert("Welcome to the App")}>
+                <TouchableOpacity 
+                  style = {styles.buttonContainer} 
+                  onPress={() => navigate('DrawerStack')}
+                >
                     <Text style = {styles.buttonText}>Login</Text>
                 </TouchableOpacity>
             </View>
@@ -49,3 +82,4 @@ const styles = StyleSheet.create({
         fontWeight: '700'
     }
 })
+export default withNavigation(LoginForm);
